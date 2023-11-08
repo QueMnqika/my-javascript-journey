4dce907495f1f7e41784eb75f57caeb1c085c066
Que Mnqika <qaqambilemnqika23@gmail.com>
2023-11-08 09:56:32 +0200

    TASK 1 COMPLETED

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// TASK 1: VARIABLES & OPERATORS
+
+let language = 'JavaScript';
+let score = 10;
+let length = 10;
+let width = 5;
+
+// Area of triangle
+
+let area = length * width;
+
+console.log(`The area of the rectangle is ${area}`);
+
+
+let num1 = 10;
+let num2  = 4
+let remainder = 10%4;
+let divisionOutput = num1/num2;
+let remainder2 = num1%num2;
+let output = divisionOutput = remainder;
+
+console.log(`The Remainder = ${remainder}`);
+
+console.log(`The Division output is ${output}`);
